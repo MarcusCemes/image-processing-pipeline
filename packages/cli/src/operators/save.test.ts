f3e622da452165bf756ca3522d852ac47631b48c
Marcus Cemes <marcus.cemes@gmail.com>
2022-07-19 17:09:59 +0200

    chore: finalize variable-extension PR

--- packages/cli/src/operators/save.test.ts
+++ packages/cli/src/operators/save.test.ts
@@ -5,13 +5,14 @@
  * LICENSE file in the root directory of this source tree.
  */
 
-import { PassThrough } from "stream";
+import { BaseMetadata, Metadata, MetadataFragment } from "@ipp/common";
+import { createHash, randomBytes } from "crypto";
 import { Stats } from "fs";
-import { CompletedTask, isCompletedTask, isSavedResult, SavedResult } from "./types";
-import { saveImages } from "./save";
-import { createObjectStream } from "../lib/stream/object_stream";
+import { PassThrough } from "stream";
 import { Exception } from "../../../common/src/exception";
-import { Metadata, PrimitiveValue } from "@ipp/common";
+import { createObjectStream } from "../lib/stream/object_stream";
+import { saveImages } from "./save";
+import { CompletedTask, isCompletedTask, isSavedResult, SavedResult } from "./types";
 
 jest.mock("fs", () => ({
   createReadStream: jest.fn(() => {
@@ -35,91 +36,78 @@ jest.mock("fs", () => ({
   },
 }));
 
-const awaitTaskSource = async (source: AsyncIterable<Exception | CompletedTask | SavedResult>) => {
-  const resolved: {
-    completed: CompletedTask[];
-    saved: SavedResult[];
-    error: Exception[];
-  } = {
-    completed: [],
-    error: [],
-    saved: [],
-  };
-  for await (const tsk of source) {
-    if (isCompletedTask(tsk)) resolved.completed.push(tsk);
-    else if (isSavedResult(tsk)) resolved.saved.push(tsk);
-    else if (tsk instanceof Exception) resolved.error.push(tsk);
+async function awaitTaskSource(source: AsyncIterable<Exception | CompletedTask | SavedResult>) {
+  const completed: CompletedTask[] = [];
+  const saved: SavedResult[] = [];
+  const error: Exception[] = [];
+
+  for await (const task of source) {
+    if (isCompletedTask(task)) completed.push(task);
+    else if (isSavedResult(task)) saved.push(task);
+    else if (task instanceof Exception) error.push(task);
   }
-  return resolved;
-};
 
-const asyncIterableImageTasks = async function* (format: string) {
-  const metadata: Metadata<Record<string, PrimitiveValue>> = {
-    current: {
-      name: "test",
-      channels: 3,
-      format,
-      hash: "1337",
-      height: 128,
-      width: 128,
-    },
-    source: {
-      name: "test",
-      channels: 3,
-      format: "png",
-      hash: "1338",
-      height: 128,
-      width: 128,
-    },
+  return [completed, saved, error] as const;
+}
+
+async function* asyncIterableImageTasks(format: string) {
+  const metadata: Metadata = {
+    source: fragment("png", "source"),
+    current: fragment(format, "current"),
   };
 
+  const source = { buffer: randomBytes(16), metadata };
+  const formats = [{ data: { buffer: randomBytes(16), metadata }, saveKey: true }];
   const sequence: CompletedTask[] = [
     {
       file: "test.png",
-      result: {
-        source: {
-          buffer: Buffer.from("content"),
-          metadata,
-        },
-        formats: [{ data: { buffer: Buffer.from("content"), metadata }, saveKey: true }],
-      },
+      result: { source, formats },
       root: "/input/root",
     },
   ];
 
   for (const item of sequence) yield item;
-};
+}
 
-describe("save.ts", () => {
+function fragment(format: string, type: string): BaseMetadata & MetadataFragment {
+  return {
+    name: "file_name",
+    channels: 3,
+    format,
+    hash: hash(type),
+    width: 128,
+    height: 128,
+  };
+}
+
+function hash(text: string): string {
+  return createHash("md5").update(text).digest().toString("hex");
+}
+
+describe("file saving", () => {
   afterEach(() => jest.clearAllMocks());
 
-  test("support avif extension in save", async () => {
-    const stream = createObjectStream(asyncIterableImageTasks("avif")).pipe(
-      saveImages("path/to/output", true)
-    );
-
-    const result = await awaitTaskSource(stream);
-    expect(result.error).toHaveLength(0);
-    expect(result.completed).toHaveLength(0);
-    expect(result.saved).toHaveLength(1);
-
-    expect(result.saved[0].savedResult.formats[0].data.metadata.current.base).toStrictEqual(
-      "test-1337.avif"
-    );
+  test("support the .avif extension", async () => {
+    await testSave("avif");
   });
 
-  test("support technically any extension, e.g. heif", async () => {
-    const stream = createObjectStream(asyncIterableImageTasks("heif")).pipe(
-      saveImages("path/to/output", true)
-    );
-
-    const result = await awaitTaskSource(stream);
-    expect(result.error).toHaveLength(0);
-    expect(result.completed).toHaveLength(0);
-    expect(result.saved).toHaveLength(1);
-
-    expect(result.saved[0].savedResult.formats[0].data.metadata.current.base).toStrictEqual(
-      "test-1337.heif"
-    );
+  test("support unknown extensions", async () => {
+    await testSave("unknown");
   });
+
+  async function testSave(extension: string) {
+    const tasks = asyncIterableImageTasks(extension);
+    const stream = createObjectStream(tasks).pipe(saveImages("path/to/output", true));
+
+    const [completed, saved, error] = await awaitTaskSource(stream);
+
+    expect(error).toHaveLength(0);
+    expect(completed).toHaveLength(0);
+    expect(saved).toHaveLength(1);
+
+    const { savedResult } = saved[0];
+    const { metadata } = savedResult.formats[0].data;
+    const h = hash("current").substring(0, 8);
+    expect(metadata.current.base).toStrictEqual(`file_name-${h}.${extension}`);
+  }
 });
